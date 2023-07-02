69cffbde2b51e0b6da0f4fdb7e426368e28a15ad
Ammar M. Kamel <ammarkamel9899@gmail.com>
2023-07-02 23:13:47 +0300

    Used JS to add data dynamically

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const imgElements = document.querySelectorAll("img");
+const spanElements = document.querySelectorAll("#summary span");
+
+fetch("../data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    for (let i = 0; i < 4; i++) {
+      imgElements[i].src = data[i].icon;
+      imgElements[i].insertAdjacentHTML("afterend", data[i].category);
+      spanElements[i].innerText = data[i].score;
+    }
+  })
+  .catch((e) => console.log(e));
